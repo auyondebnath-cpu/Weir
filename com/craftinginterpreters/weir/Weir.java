01951864185160bdb6c29234f05be1fc0171d14f
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-08-22 19:15:35 +1000

    Second Commit - Improvement of main

---
+++ com/craftinginterpreters/weir/Weir.java
@@ -0,0 +1,32 @@
+package com.craftinginterpreters.weir;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Weir {
+    static boolean hadError = false;
+
+    public static void main (String[] args) throws IOException{
+        if(args.length == 0){
+            runPrompt();
+            return;
+        }
+
+        StringBuilder combined = new StringBuilder();
+        for(String path: args){
+            byte[] bytes = Files.readAllBytes(Paths.get(path));
+            combined.append(new String(bytes, StandardCharsets.UTF_8));
+            combined.append("\n");
+        }
+
+        run(combined.toString());
+
+        if (hadError) System.exit(65);
+    }
+
+}
