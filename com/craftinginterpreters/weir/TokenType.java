3e4f4c116e454094e8ff50603d868f257bcfbe61
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-08-23 18:46:18 +1000

    Add class TokenType

---
+++ com/craftinginterpreters/weir/TokenType.java
@@ -0,0 +1,27 @@
+package com.craftinginterpreters.weir;
+
+enum TokenType{
+    //Single Character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, MINUS, PLUS, SEMICOLON, STAR, SLASH,
+    
+    //One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    //Literals
+    IDENTIFIER, STRING, NUMBER, 
+
+    //keywords
+    AND, ELSE, FALSE, IF, FOR, NIL, OR,
+    PRINT, RETURN, TRUE, WHILE,
+
+    //Weir specific keywords for river system
+    RIVER, ROOT, DAM, FLOWS, WHEN,
+
+    EOF
+}
+
+
