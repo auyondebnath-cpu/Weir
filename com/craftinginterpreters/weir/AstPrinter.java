6e9c4dd6de2915c3c36fe21e3a83f97cb46489c6
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-09-25 21:36:16 +1000

    Update AstPrinter.java for the specifying the river grammar

--- com/craftinginterpreters/weir/AstPrinter.java
+++ com/craftinginterpreters/weir/AstPrinter.java
@@ -1,19 +1,35 @@
 package com.craftinginterpreters.weir;
 
-class AstPrinter implements Expr.Visitor<String> {
+import java.util.List;
+
+
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
+
+    String print(List<Stmt> statements) {
+        StringBuilder builder = new StringBuilder();
+        for (Stmt stmt : statements) {
+            builder.append(stmt.accept(this));
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
     String print(Expr expr) {
         return expr.accept(this);
     }
+
 
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
+
     @Override
     public String visitGroupingExpr(Expr.Grouping expr) {
         return parenthesize("group", expr.expression);
     }
+
 
     @Override
     public String visitLiteralExpr(Expr.Literal expr) {
@@ -21,13 +37,92 @@ class AstPrinter implements Expr.Visitor<String> {
         return expr.value.toString();
     }
 
+
     @Override
     public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
+
+    @Override
+    public String visitFlowsExpr(Expr.Flows expr) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(flows ");
+        builder.append(expr.source.accept(this));
+        builder.append(" ");
+        builder.append(expr.target.lexeme);
+        builder.append(")");
+        return builder.toString();
+    }
+
+
+    @Override
+    public String visitRootStmt(Stmt.Root stmt) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(root ").append(stmt.name.lexeme);
+        if (stmt.size != null) {
+            builder.append(" ").append(stmt.size.accept(this));
+        }
+        builder.append(")");
+        return builder.toString();
+    }
+
+
+    @Override
+    public String visitRiverDeclStmt(Stmt.RiverDecl stmt) {
+        return "(river " + stmt.name.lexeme + " = " + stmt.value.accept(this) + ")";
+    }
+
+
+    @Override
+    public String visitDamStmt(Stmt.Dam stmt) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(dam ").append(stmt.name.lexeme).append(" ");
+        for (Stmt rule : stmt.rules) {
+            builder.append(rule.accept(this)).append(" ");
+        }
+        builder.append(stmt.defaultRule.accept(this));
+        builder.append(")");
+        return builder.toString();
+    }
+
+
+    @Override
+    public String visitDamRuleStmt(Stmt.DamRule stmt) {
+        if (stmt.condition == null) {
+            return "(default " + stmt.result.accept(this) + ")";
+        }
+        return "(when " + stmt.condition.accept(this) + " " + stmt.result.accept(this) + ")";
+    }
+
+
+    @Override
+    public String visitConnectStmt(Stmt.Connect stmt) {
+        return "(flows " + stmt.source.lexeme + " " + stmt.target.lexeme + ")";
+    }
+
+
+    @Override
+    public String visitPrintStmt(Stmt.Print stmt) {
+        return "(print " + stmt.expression.accept(this) + ")";
+    }
+
+
+    @Override
+    public String visitExpressionStmt(Stmt.Expression stmt) {
+        return stmt.expression.accept(this);
+    }
+
+
     private String parenthesize (String name, Expr... exprs){
         StringBuilder builder = new StringBuilder();
+
 
         builder.append("(").append(name);
         for(Expr expr:exprs){
@@ -36,8 +131,10 @@ class AstPrinter implements Expr.Visitor<String> {
         }
         builder.append(")");
 
+
         return builder.toString();
     }
+
 
     public static void main(String[] args){
         Expr expression = new Expr.Binary(
---
+++ com/craftinginterpreters/weir/Stmt.java
@@ -0,0 +1,120 @@
+package com.craftinginterpreters.weir;
+
+import java.util.List;
+
+abstract class Stmt {
+  interface Visitor<R> {
+    R visitRootStmt(Root stmt);
+    R visitRiverDeclStmt(RiverDecl stmt);
+    R visitDamStmt(Dam stmt);
+    R visitDamRuleStmt(DamRule stmt);
+    R visitConnectStmt(Connect stmt);
+    R visitPrintStmt(Print stmt);
+    R visitExpressionStmt(Expression stmt);
+  }
+  static class Root extends Stmt {
+    Root(Token name, Expr size) {
+      this.name = name;
+      this.size = size;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitRootStmt(this);
+    }
+
+    final Token name;
+    final Expr size;
+  }
+
+  static class RiverDecl extends Stmt {
+    RiverDecl(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitRiverDeclStmt(this);
+    }
+
+    final Token name;
+    final Expr value;
+  }
+
+  static class Dam extends Stmt {
+    Dam(Token name, List<Stmt> rules, Stmt defaultRule) {
+      this.name = name;
+      this.rules = rules;
+      this.defaultRule = defaultRule;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitDamStmt(this);
+    }
+
+    final Token name;
+    final List<Stmt> rules;
+    final Stmt defaultRule;
+  }
+
+  static class DamRule extends Stmt {
+    DamRule(Expr condition, Expr result) {
+      this.condition = condition;
+      this.result = result;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitDamRuleStmt(this);
+    }
+
+    final Expr condition;
+    final Expr result;
+  }
+
+  static class Connect extends Stmt {
+    Connect(Token source, Token target) {
+      this.source = source;
+      this.target = target;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitConnectStmt(this);
+    }
+
+    final Token source;
+    final Token target;
+  }
+
+  static class Print extends Stmt {
+    Print(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+
+    final Expr expression;
+  }
+
+  static class Expression extends Stmt {
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+
+    final Expr expression;
+  }
+
+
+  abstract <R> R accept(Visitor<R> visitor);
+}
